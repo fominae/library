4783e22f760b4f20ccecdf15bccf91c9f705f61e
Your Name <your_email@whatever.com>
2023-10-31 00:17:23 +0700

    Add file

--- locallibrary/locallibrary/catalog/admin.py
+++ locallibrary/locallibrary/catalog/admin.py
@@ -34,12 +34,11 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
 
-
-     fieldsets = (
-            (None, {
-                'fields': ('book','imprint', 'id')
-            }),
-            ('Availability', {
-                'fields': ('status', 'due_back')
-            }),
-        )
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
