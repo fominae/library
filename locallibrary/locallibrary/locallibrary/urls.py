3b959fda6266ad7349df46c6a2255408c653b41e
Your Name <your_email@whatever.com>
2023-10-30 21:46:37 +0700

    Add file

--- locallibrary/locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/locallibrary/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+     path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
