93506b6d7d3ff87604b05687321b30950700995f
Your Name <your_email@whatever.com>
2023-10-31 16:37:49 +0700

    Конфигурация URL-адресов

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -18,3 +18,8 @@ urlpatterns = [
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
