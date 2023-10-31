fd10402238152ea52befca467deb8c77ad784aea
Your Name <your_email@whatever.com>
2023-10-31 11:24:59 +0700

    Создание получения числа визитов

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -11,13 +11,16 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
-
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},  # num_visits appended
     )
 
 class BookListView(generic.ListView):
