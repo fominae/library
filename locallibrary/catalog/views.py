139cf16d9331f4d57d2a97c043ec4b893bf38d52
Your Name <your_email@whatever.com>
2023-10-31 13:45:05 +0700

    Добавления представления для получения списка всех книг

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -48,4 +48,4 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back'
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
