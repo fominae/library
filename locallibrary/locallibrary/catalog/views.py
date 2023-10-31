ebf69e77a3d3ad2c508186c14ea3bc63927872f9
Your Name <your_email@whatever.com>
2023-10-31 09:03:40 +0700

    Add file

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
-
+from django.views import generic
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
@@ -20,3 +20,5 @@ def index(request):
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
 
+class BookListView(generic.ListView):
+    model = Book
