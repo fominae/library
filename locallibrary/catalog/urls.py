a51e1f7f9735dd745fb8d2831d0381ea5eab7e29
Your Name <your_email@whatever.com>
2023-10-31 13:48:19 +0700

    URL-адрес для заёмных книг

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -14,3 +14,7 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 
 ]
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
